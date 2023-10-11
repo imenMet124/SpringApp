f1609b5d9f911d36b1ba84c253acbe524e8c957a
imenMet124 <methniimen@gmail.com>
2023-10-12 00:29:41 +0200

    ajout car

--- src/main/java/com/imen/garageApp/controller/GarageController.java
+++ src/main/java/com/imen/garageApp/controller/GarageController.java
@@ -1,15 +1,17 @@
 package com.imen.garageApp.controller;
 
+import com.imen.garageApp.model.Car;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class GarageController {
-    @RequestMapping("/hello")
+    @RequestMapping("/car")
 
-    public String sayHello (){
-        return "hello world";
+    public Car getCard(){
+        Car myCar= new Car("laguna","renault",2004,Car.Color.RED);
+        return myCar;
     }
 
 }
---
+++ src/main/java/com/imen/garageApp/model/Car.java
@@ -0,0 +1,55 @@
+package com.imen.garageApp.model;
+
+public class Car {
+
+    public enum Color{
+        RED,
+        BLUE,
+        GREEN,
+        YELLOW
+    }
+
+    private String model;
+    private String brand;
+    private int year;
+    private Color color;
+
+    public Car(String model, String brand, int year, Color color) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
